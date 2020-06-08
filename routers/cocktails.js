0c18e3f4edb3210d4d3d83d9a043ebfcd78b7d0a
unknown <hieupvd00585@fpt.edu.vn>
2020-06-08 16:43:37 +0700

    change file

--- routers/cocktails.js
+++ routers/cocktails.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const getData = require('../controller/filter')
-const createCocktail = require('../controller/create')
-const getId = require('../controller/read')
-const deleteCocktail = require('../controller/delete')
-const updateAll = require('../controller/update')
-const updateOneField = require('../controller/updateOneField')
+const {
+    updateAll,
+    getData,
+    getId,
+    deleteCocktail,
+    createCocktail
+
+} = require('../controller/cocktailController')
 
 // Get all 
 router.get('/', getData)
@@ -19,9 +21,6 @@ router.get('/:cocktailId', getId)
 
 // Delete a cocktail
 router.delete('/:cocktailId', deleteCocktail)
-
-//Specific  update 
-router.patch('/:cocktailId', updateOneField)
 
 // Update all 
 router.put('/:cocktailId', updateAll)
